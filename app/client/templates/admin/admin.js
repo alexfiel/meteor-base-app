590c83629c2b967ef111fc67099c7eec5c780186
Jan Maghuyop <jangmaghuyop@gmail.com>
2016-03-26 12:09:26 +0800

    made admin nav, generated roles and users template

--- app/client/templates/admin/admin.js
+++ app/client/templates/admin/admin.js
@@ -21,3 +21,38 @@ Template.Admin.onRendered(function () {
 
 Template.Admin.onDestroyed(function () {
 });
+
+
+/*****************************************************************************/
+/* AdminNav: Event Handlers */
+/*****************************************************************************/
+Template.AdminNav.events({
+  'click .adminLinks':function (event) {
+    $('.adminLinks').removeClass('active');
+  },
+  'click #usersLink':function (event) {
+    $('#usersLink').addClass('active');
+  },
+  'click #rolesLink':function (event) {
+    $('#rolesLink').addClass('active');
+  }
+});
+
+/*****************************************************************************/
+/* AdminNav: Helpers */
+/*****************************************************************************/
+Template.AdminNav.helpers({
+});
+
+/*****************************************************************************/
+/* AdminNav: Lifecycle Hooks */
+/*****************************************************************************/
+Template.AdminNav.onCreated(function () {
+});
+
+Template.AdminNav.onRendered(function () {
+  $('#usersLink').addClass('active');
+});
+
+Template.AdminNav.onDestroyed(function () {
+});
